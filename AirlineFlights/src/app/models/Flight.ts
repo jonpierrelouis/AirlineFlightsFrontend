1520ec2c30d5b51caa7c2c2aa408edc3a0fd5c3b
Jonathan Pierre-Louis <jopierrelouis@gmail.com>
2022-11-09 14:23:30 -0500

    added Flight model

--- AirlineFlights/src/app/models/Flight.ts
+++ AirlineFlights/src/app/models/Flight.ts
@@ -2,10 +2,14 @@ export default class Flight {
     id: number;
     departureCity: string;
     arrivalCity: string;
+    airline: string;
+    cost: number;
 
-    constructor(id: number, departureCity: string, arrivalCity: string){
+    constructor(id: number, departureCity: string, arrivalCity: string, airline: string, cost: number){
         this.id = id;
         this.departureCity = departureCity;
         this.arrivalCity = arrivalCity;
+        this.airline = airline;
+        this.cost = cost;
     }
 }
